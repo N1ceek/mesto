d5ab67e4825f40e70a4df8d36e2e68cfaef6e23b
N1ceek <119848140+N1ceek@users.noreply.github.com>
2023-05-19 00:07:00 +0300

    боже сработай

--- index.js
+++ index.js
@@ -22,35 +22,3 @@ function handleFormSubmit (evt){
   document.querySelector(`.profile__workplace`).textContent = jobInput.value;
   popupOpen.classList.remove(`popup_opened`);
 }
- 
-
-
-
-
-
-
-
-
-//let edit = document.querySelector(`.profile__edit-button`);
-//let popupOpen = document.querySelector('.popup');
-//edit.addEventListener(`click`, function() {
- // popupOpen.classList.add(`popup_opened`)
-//});
-//let editClose = document.querySelector(`.popup__close`);
-//editClose.addEventListener(`click`, function() {
-//  popupOpen.classList.remove(`popup_opened`)
-//});
-//edit.addEventListener(`click`, function() {
- // popupOpen.classList.add(`popup_opened`)
-//})
-
-//let sumbit = document.querySelector(`.form__save-button`).onclick = handleFormSubmit;
-
-//function handleFormSubmit(evt) {
-//  evt.preventDefault();
-//  let nameInput = document.querySelector(`.form__input_type_user-name`).value;
-//  document.querySelector(`.profile__name`).textContent = nameInput;
-//  let jobInput = document.querySelector(`.form__input_type_about`).value;
- // document.querySelector(`.profile__workplace`).textContent = jobInput;
- // popupOpen.classList.remove(`popup_opened`);
-//};
