edfa5fc47d413e82ac90ae900453ab3d4fafe06c
N1ceek <119848140+N1ceek@users.noreply.github.com>
2023-05-20 19:20:56 +0300

    20.05.23.1

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
+// let 
+
+
 let edit = document.querySelector(`.profile__edit-button`);
 let popupOpen = document.querySelector('.popup');
-edit.addEventListener(`click`, function() {
-  popupOpen.classList.add(`popup_opened`)
-});
 let editClose = document.querySelector(`.popup__close`);
-editClose.addEventListener(`click`, function() {
-  popupOpen.classList.remove(`popup_opened`)
-});
-edit.addEventListener(`click`, function() {
-  popupOpen.classList.add(`popup_opened`)
-});
 let formElement = document.querySelector(`.form`);
 let nameInput = formElement.querySelector(`.form__input_type_user-name`);
 let jobInput = formElement.querySelector(`.form__input_type_about`);
-formElement.addEventListener('submit', handleFormSubmit);
+let profileName = document.querySelector(`.profile__name`);
+let profielWorkplace = document.querySelector(`.profile__workplace`);
+
+// function
+
+
+function openEdit() {
+  popupOpen.classList.add(`popup_opened`)
+  nameInput.textContent = profileName.value
+  
+
+};
+function closeEdit() {
+  popupOpen.classList.remove(`popup_opened`)
+};
 function handleFormSubmit (evt){
   evt.preventDefault();
-  console.log(nameInput.value)
-  document.querySelector(`.profile__name`).textContent = nameInput.value;
-  console.log(jobInput.value)
-  document.querySelector(`.profile__workplace`).textContent = jobInput.value;
+  nameInput.value
+  profileName.textContent = nameInput.value;
+  jobInput.value
+  profielWorkplace.textContent = jobInput.value;
   popupOpen.classList.remove(`popup_opened`);
-}
+};
+
+
+// listener
+
+
+edit.addEventListener(`click`, openEdit);
+editClose.addEventListener(`click`, closeEdit);
+formElement.addEventListener('submit', handleFormSubmit);
