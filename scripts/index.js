9c747e8749289f9d2f4125e9bc538d555db88eec
N1ceek <119848140+N1ceek@users.noreply.github.com>
2023-05-21 12:27:31 +0300

    21.05.23.1

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+// let 
+
+
+let edit = document.querySelector(`.profile__edit-button`);
+let popupOpen = document.querySelector('.popup');
+let editClose = document.querySelector(`.popup__close`);
+let formElement = document.querySelector(`.form`);
+let nameInput = formElement.querySelector(`.form__input_type_user-name`);
+let jobInput = formElement.querySelector(`.form__input_type_about`);
+let profileName = document.querySelector(`.profile__name`);
+let profielWorkplace = document.querySelector(`.profile__workplace`);
+
+// function
+
+
+function openEdit() {
+  popupOpen.classList.add(`popup_opened`)
+  nameInput.value = profileName.textContent
+  jobInput.value = profielWorkplace.textContent
+};
+function closeEdit() {
+  popupOpen.classList.remove(`popup_opened`)
+};
+function handleFormSubmit (evt){
+  evt.preventDefault();
+  nameInput.value
+  profileName.textContent = nameInput.value;
+  jobInput.value
+  profielWorkplace.textContent = jobInput.value;
+  popupOpen.classList.remove(`popup_opened`);
+};
+
+
+// listener
+
+
+edit.addEventListener(`click`, openEdit);
+editClose.addEventListener(`click`, closeEdit);
+formElement.addEventListener('submit', handleFormSubmit);
